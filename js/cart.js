979049369abc348371e71ddb2421211890ba7aa1
Shevchenko Andrey <demorest49de@gmail.com>
2023-10-24 18:55:17 +0300

    Changes cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,14 @@
 'use strict';
 
-const Cart = function (goods = []) {
+const Cart = function ([goods = [], totalPrice = 0, count = 0]) {
     this.goods = goods;
-    this.totalPrice = 0;
-    this.count = 0;
+    this.totalPrice = totalPrice;
+    this.count = count;
 };
 
-
-Cart.prototype.addGoods = function (name = '', price = 1, amount = 1) {
-    if (typeof (name) === 'string' && name.length >= 3
-        && typeof (price) === 'number' && price >= 1
-        && typeof (amount) === 'number' && amount >= 1) {
-        this.goods.push({name, price, amount});
-    } else {
-        console.error('wrong arguments type or value!');
-    }
-    this.increaseCount(amount);
+Cart.prototype.addGoods = function (item) {
+    this.goods.push(item);
+    this.increaseCount(item.amount);
     this.calculateGoodsPrice();
 };
 
@@ -40,76 +33,62 @@ Cart.prototype.clear = function () {
 
 Cart.prototype.print = function () {
     console.log(JSON.stringify(this.goods));
-    console.log(`Общая стоимость корзины: ${this.totalPrice} рублей`);
+    console.log(`Cart total: ${this.totalPrice} $`);
+    console.log(`Items total: ${this.count}`);
 };
 
-const cart1 = new Cart();
+const cart1 = new Cart([]);
 cart1.clear();
-cart1.addGoods('apple watch', 500.5, 2);
-cart1.addGoods('apple 15', 1500, 3);
+cart1.addGoods({name: 'apple watch', price: 500.5, amount: 2});
+cart1.addGoods({name: 'apple 15', price: 1500, amount: 3});
 cart1.getTotalPrice();
 cart1.print();
 
 const Goods = function (name = '', price = 1, discount = 0) {
-    this.name = price;
-    this.price = name;
+    this.name = name;
+    this.price = price;
     this.discount = discount;
 };
 
-const FoodGoods = function (name = '', price = 1, discount = 1, calories = 1) {
+const FoodGoods = function (
+    name = '',
+    price = 1,
+    discount = 0,
+    calories = 1) {
     Goods.call(this, name, price, discount);
     this.calories = calories;
+    this.amount = 1;
 };
 
-const ClothingGoods = function (name = '', price = 1, discount = 1, material = '') {
+const ClothingGoods = function (
+    name = '',
+    price = 1,
+    discount = 0,
+    material = '') {
+    Goods.call(this, name, price, discount);
     this.material = material;
+    this.amount = 1;
 };
-const TechnicsGoods = function (name = '', price = 1, discount = 1, type = '') {
+const TechnicsGoods = function (
+    name = '',
+    price = 1,
+    discount = 0,
+    type = '') {
+    Goods.call(this, name, price, discount);
     this.type = type;
+    this.amount = 1;
 };
+
 Object.setPrototypeOf(FoodGoods.prototype, Goods.prototype);
 Object.setPrototypeOf(ClothingGoods.prototype, Goods.prototype);
 Object.setPrototypeOf(TechnicsGoods.prototype, Goods.prototype);
 
-// const Car = function (brand, model, maxTank) {
-//     this.brand = brand;
-//     this.model = model;
-//     this.maxTank = maxTank;
-//     this.nowTank = Math.floor(Math.random() * this.maxTank);
-// };
-//
-// // статичный метод класса Car
-// Car.from = function ({brand, model, maxTank}) {
-//     return new Car({brand, model, maxTank});
-// };
-//
-// Car.prototype.needPetrol = function () {
-//     console.log(' this.maxTank - this.nowTank: ', this.maxTank - this.nowTank);
-//     return (this.maxTank - this.nowTank);
-// };
-//
-// Car.prototype.fillUp = function () {
-//     return this.nowTank = this.maxTank;
-// };
-//
-// Car.from = function ({brand, model, maxTank}) {
-//     return new Car({brand, model, maxTank});
-// };
-//
-// const PassangerCar = function (brand, model, maxTank, typeFuel = 'petrol') {
-//     Car.call(this, brand, model, maxTank);
-//     this.typeFuel = typeFuel;
-//     this.typeCar = 'passenger';
-// };
-//
-// PassangerCar.contextFrom = function () {
-//     console.log(this);
-// };
-//
-// const bmw = new PassangerCar('bmw', 'x5', 60);
-//
-//
-// Object.setPrototypeOf(PassangerCar.prototype, Car.prototype);
-// console.log(' bmw: ', bmw);
-// bmw.needPetrol();
-// PassangerCar.contextFrom();
+const food = new FoodGoods('grape', 18, 0, 100);
+const clothes = new ClothingGoods('coat', 3000, 3, 'cashmere');
+const technics = new TechnicsGoods('phone', 1000, 2, 'connection');
+console.log(' food: ', food);
+console.log(' food: ', food instanceof Goods);
+cart1.addGoods(food);
+cart1.addGoods(clothes);
+cart1.addGoods(technics);
+cart1.print();
